7b17cfa1061550d2110cef570df720ed9b7fc05f
Thiago <thiagogabrielmta@outlook.com.br>
2024-09-10 13:54:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+import Board from './components/squareComponent/square.component'
+
+function App() {
+
+  return (
+    <>
+      <Board/>
+    </>
+  )
+}
+
+export default App
